358165a2cdeb20cf4b1e2145a82001356ad80bdf
RanaShiva <shivamrana.4wrk@gmail.com>
2022-04-12 09:52:32 +0530

    Added javascript(validation + storage) in 5th project

--- 5-Website Validation/script.js
+++ 5-Website Validation/script.js
@@ -1,9 +1,9 @@
 function storeDetails()
 {
-    var password = document.getElementById("password").value;
+    let password = document.getElementById("password").value;
     sessionStorage.setItem("password",password);
 
-    var email = document.getElementById("email").value;
+    let email = document.getElementById("email").value;
     sessionStorage.setItem("uname",email);
 
     alert("User registered!");
@@ -11,9 +11,15 @@ function storeDetails()
 
 function checkDetails()
 {
-    var password = document.getElementById("password").value;
+    let password = document.getElementById("password").value;
+    let cpassword = document.getElementById("cpass").value;
+    if(password != cpassword)
+    {
+        alert("Password does not match!");
+        return false;
+    }
 
-    var name = document.getElementById("name").value;
+    let name = document.getElementById("name").value;
 
     if(sessionStorage.getItem("password")==password && sessionStorage.getItem("uname") == name)
     {
@@ -22,5 +28,64 @@ function checkDetails()
     else
     {
         alert("Invalid Credentials!");
+        return false;
     }
+}
+function formValidate()
+{
+    //Name validation
+    let uname = document.getElementById("uname").value;
+    let namePattern = /[A-Za-z]{3,25}/;
+    if(uname.match(namePattern)==null){
+        alert("Name should have more than 2 characters and less than 25.");
+        return false;
+    }
+
+    //Email validation
+    let email = document.getElementById("email").value;
+    let emailPattern = /[A-Za-z0-9.+_-]+@[A-Za-z0-9.]/;
+    if(email.match(emailPattern)==null)
+    {
+        alert("Invalid email format!");
+        return false;
+    }
+
+    //Password validation
+    /*8 characters long
+    must contain an uppercase, lowercase and a number*/
+
+    let password = document.getElementById("password").value;
+    let cpassword = document.getElementById("cpass").value;
+    if(password != cpassword)
+    {
+        alert("Password does not match!");
+        return false;
+    }
+    let passwordPattern = /[A-Z]+[a-z]+[0-9]/g;
+    if(password.match(passwordPattern)==null && password.length <8)
+    {
+        alert("Password must contain one uppercase, one lowercase and one number at least and should have atleast a length of 8");
+        return false;
+    }
+
+    //Captcha validation
+    
+    let captcha = document.getElementById("code").value;
+    if(captcha!=document.getElementById("captcha").innerHTML)
+    {
+        alert("Captcha did not match, try again!");
+        return false;
+    }
+
+    storeDetails();
+}
+function captchaGenerator()
+{
+    let random = Math.floor(Math.random()*8999)+1000;
+    document.getElementById("captcha").innerHTML=random;
+}
+
+function paymentValidation()
+{
+    
 }
